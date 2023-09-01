db915bf017829388c8b1e5ee49d707325ce29335
Mahmud sajib <mahmudsajib082@gmail.com>
2023-09-02 02:28:43 +0600

    cocktailslice crrate and store

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+    <div className="navbar navbar-light bg-light fixed-top">
+
+      <div className="container-fluid">
+        <span className='navbar-brand mb0 h1 header-title text-success'>
+            Find your favorite  Cocktail
+        </span>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Header
